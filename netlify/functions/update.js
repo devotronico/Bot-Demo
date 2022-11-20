d805005e5d09bc4c374dc4b64c7cc423087bf917
redeluni <dmanzi83@gmail.com>
2022-11-20 13:17:29 +0100

    test per provare a leggere la var BOT_TOKEN

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
 exports.handler = async (event) => {
-  console.log("Received an update from Telegram!", event.body);
+  console.log(`Received ${process.env.BOT_TOKEN}`, event.body);
 
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     chat_id: JSON.parse(event.body).message.chat.id,
